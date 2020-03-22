82f7cc1d45f6629f71b2d24c72188c8d1c9143d0
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2020-03-22 16:25:18 -0700

    refactor out functions

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const snapImage = (video) => {
     return dataUrlImage
   }
 }
-
+//  make a different size version of this image as represented in the dataUrlImage
 const makeImg = (dataUrlImage, width, height) => {
   const img = document.createElement('img')
   img.src = dataUrlImage
   img.width = width;
   img.height = height;
-  document.getElementsByTagName('div')[0].appendChild(img)
+  document.getElementsByTagName('div')[0].appendChild(img) // TODO move this out of this function
 }
 const imageConfig = (w, h) => {
   const maxWidth = 200
@@ -37,50 +37,61 @@ const imageConfig = (w, h) => {
     }
   }
 }
-document.getElementsByTagName('input')[0].addEventListener('change', (event) => {
-  const file = event.target.files[0]
+const handleFileInput = async (files) => {
+  const file = files[0]
   const fileReader = new FileReader()
   if (file.type.match('image')) {
     fileReader.onload = () => {
-      const img = new Image()
-      img.onload = function () {
-        const ic = imageConfig( img.width, img.height )
-        makeImg(fileReader.result, ic.width(), ic.height())
-      }
-      img.src = fileReader.result
+      smallImage(fileReader.result)
+      console.log(fileReader.result)
     }
     fileReader.readAsDataURL(file)
   } else {
     fileReader.onload = () => {
-      const blob = new Blob([fileReader.result], { type: file.type })
-      const url = URL.createObjectURL(blob)
-      const video = document.createElement('video')
-      const timeupdate = () => {
-        const dataUrlImage = snapImage(video)
-        if (dataUrlImage) {
-          const ic = imageConfig( video.videoWidth, video.videoHeight )
-          makeImg(dataUrlImage, ic.width(), ic.height())
-          video.pause()
-          URL.revokeObjectURL(url)
-        }
-      }
-      video.addEventListener('loadeddata', () => {
-        const dataUrlImage = snapImage(video)
-        if (dataUrlImage) {        
-          const ic = imageConfig( video.videoWidth, video.videoHeight )
-          makeImg(dataUrlImage, ic.width(), ic.height())
-          video.removeEventListener('timeupdate', timeupdate)
-          URL.revokeObjectURL(url)
-        }
-      })
-      video.addEventListener('timeupdate', timeupdate)
-      video.preload = 'metadata'
-      video.src = url
-      // Load video in Safari / IE11
-      video.muted = true
-      video.playsInline = true
-      video.play()
+      console.log(fileReader.result)
+      smallVideoImage(fileReader.result)
     }
     fileReader.readAsArrayBuffer(file)
   }
-})
+}
+const smallImage = (imageDataUrl) => {
+  const img = new Image()
+  img.onload = function () {
+    const ic = imageConfig(img.width, img.height)
+    makeImg(imageDataUrl, ic.width(), ic.height())
+  }
+  img.src = imageDataUrl
+}
+const smallVideoImage = (videoDataUrl) => {
+  const blob = new Blob([videoDataUrl], { type: 'video' })
+  const url = URL.createObjectURL(blob)
+  const video = document.createElement('video')
+  const timeupdate = () => {
+    const dataUrlImage = snapImage(video)
+    if (dataUrlImage) {
+      const ic = imageConfig( video.videoWidth, video.videoHeight )
+      makeImg(dataUrlImage, ic.width(), ic.height())
+      video.pause()
+      URL.revokeObjectURL(url)
+    }
+  }
+  video.addEventListener('loadeddata', () => {
+    const dataUrlImage = snapImage(video)
+    if (dataUrlImage) {        
+      const ic = imageConfig( video.videoWidth, video.videoHeight )
+      makeImg(dataUrlImage, ic.width(), ic.height())
+      video.removeEventListener('timeupdate', timeupdate)
+      URL.revokeObjectURL(url)
+    }
+  })
+  video.addEventListener('timeupdate', timeupdate)
+  video.preload = 'metadata'
+  video.src = url
+  // Load video in Safari / IE11
+  video.muted = true
+  video.playsInline = true
+  video.play()
+}; 
+
+document.getElementsByTagName('input')[0]
+  .addEventListener('change', (e) => handleFileInput(e.target.files))
